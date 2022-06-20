6bb11fb7be13bee056b12888d54ca307cb5a41de
byoso <emailvinz@gmail.com>
2022-06-20 04:49:16 +0200

    improved installer's dependecies management

--- geninstaller/__init__.py
+++ geninstaller/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 
 # DO on version change:
 # ! keep the version up to date
-# in the plop 'installer' dependencies too !
+# in the plop/installer/installer dependencies too !
